02069f51467260ceed671ed90dd3ca62ce8b9ef9
Richard Wong <richardwong@berkeley.edu>
2017-04-25 19:59:42 -0700

    eh

--- final project/assets/js/script.js
+++ final project/assets/js/script.js
@@ -1,3 +1,102 @@
 $(document).ready(function(){
 	
+
+	$("#edward").click(function(){
+		
+		window.location.href = "https://www.instagram.com/p/BIb9eAABWT-/?taken-by=lerrlerrr";
+		
+	});
+
+	$("#oksami").click(function(){
+		
+		window.location.href = "https://www.instagram.com/p/BIb9qFRBPYb/";
+		
+	});
+	$("#oksami2").click(function(){
+		
+		window.location.href = "https://www.instagram.com/p/BIZT21wBv5h/";
+		
+	});
+	$("#flower1").click(function(){
+		
+		window.location.href = "https://www.instagram.com/p/BI5gBaihBPY/";
+		
+	});
+	$("#flower2").click(function(){
+		
+		window.location.href = "https://www.instagram.com/p/BIb9b9wBFfA/";
+		
+	});
+	$("#flower3").click(function(){
+		
+		window.location.href = "https://www.instagram.com/p/BIb9nIZhfNo/";
+		
+	});
+	$("#pixel1").click(function(){
+		
+		window.location.href = "https://www.instagram.com/p/BOk_9VngmEg/";
+		
+	});
+	$("#pixel2").click(function(){
+		
+		window.location.href = "https://www.instagram.com/p/BOk_461AiRz/";
+		
+	});
+	$("#pixel3").click(function(){
+		
+		window.location.href = "https://www.instagram.com/p/BOlABy3gm_8/";
+		
+	});
+
+
+
+
+	$(".painting").click(function(){
+		
+		$("#modal-container").show();
+				alert("wow2");
+
+		
+	});
+
+
+	$("#contact").click(function(){
+		
+		$("#modal-container").show();
+		
+	});	
+
+
+
+
+
+
+  //  $('.painting').click(function() {
+  //   $('#modal-container').show(1000, "swing");
+  // });
+
+  // $('#about').click(function() {
+  //   $('#modal-container').fadeIn(1000, "swing");
+  // });
+
+  // $('#modal-overlay').click(function() {
+  //   $('#modal-container').fadeOut(1000, "swing");
+  // });
+
+  // $('#contact').click(function() {
+  //   $('#modal-container').fadeIn(1000, "swing");
+  // });
+
+  // $('#modal-overlay-overlay').click(function() {
+  //   $('#m-container').fadeOut(1000, "swing");
+  // });
+
+
+
+
+
+
+
 });
+
+
